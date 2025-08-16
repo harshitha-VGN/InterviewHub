cbba88ba0332c11cf8f4ee64e4d638b83c2e64a3
Hansika Reddy Gurrala <143734719+hansikareddy29@users.noreply.github.com>
2025-08-17 00:12:31 +0530

    Update dashboard.jsx

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Home from './Home.jsx';
 import { ArrowRightIcon } from '@heroicons/react/24/solid';
-const API_URL = import.meta.env.CLIENT_URL || 'http://localhost:5050';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5050';
 const ProfileSidebar = () => {
 
     const [user, setUser] = useState(null);
