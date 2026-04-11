a7f2bb81a427af2d2325591396be7cd77f90130d
hansikareddy29 <gurralahansikareddy@gmail.com>
2026-04-11 12:10:15 +0530

    pdf parsing bug fixedgit add .

--- server/test-endpoint.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-require('dotenv').config();
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const resumeRoutes = require('./routes/resumeRoutes');
-app.use('/api/resume', resumeRoutes);
-app.listen(5051, () => "Test server up");
